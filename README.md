43b403c30ceec9a34bc504be801a3b25c349b83e
Aigul <aigul.sibgatulli@mail.ru>
2021-04-18 21:41:57 +0300

    create default file

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ The program parses users' statistics from https://hanab.live/, calculates percen
 ## Usage
 1. Type list of players in ```input/list_of_users.txt```
 2. Run script ```main.py```
-3. See the results in the ```output/all_stats_[timestamp].tsv```.
+3. See the results in the ```output/up_to_date_stats.tsv``` or ```output/all_stats_[timestamp].tsv```.
 
 The table structure:
 
--- main.py
+++ main.py
@@ -10,6 +10,38 @@ def r(num):
     return str(num).replace('.', ',')
 
 
+def save_to_tsv(filename, data):
+    with open(f'../output/{filename}.tsv', 'w', newline='') as file:
+        w = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        w.writerow(['Username', 'Type', 'W(%)', 'L(%)', 'W(#)', 'L(#)',
+                    'W(%, 2p)', 'L(%, 2p)', 'W(#, 2p)', 'L(#, 2p)',
+                    'W(%, 3p+)', 'L(%, 3p+)', 'W(#, 3p+)', 'L(#, 3p+)'])
+        for k, v in data.items():
+            for k1, t in v.items():
+                w.writerow([
+                    k,
+                    k1,
+                    # r(t['total_p'][0]),
+                    t['total_p'][0],
+                    # r(t['total_p'][1]),
+                    t['total_p'][1],
+                    t['total_c'][0],
+                    t['total_c'][1],
+                    # r(t['total_2p_p'][0]),
+                    t['total_2p_p'][0],
+                    # r(t['total_2p_p'][1]),
+                    t['total_2p_p'][1],
+                    t['total_2p_c'][0],
+                    t['total_2p_c'][1],
+                    # r(t['total_3p_p'][0]),
+                    t['total_3p_p'][0],
+                    # r(t['total_3p_p'][1]),
+                    t['total_3p_p'][1],
+                    t['total_3p_c'][0],
+                    t['total_3p_c'][1]]
+                )
+
+
 start = time.time()
 with open('../input/list_of_users.txt', 'r') as f:
     users = [line.rstrip() for line in f.readlines()]
@@ -23,39 +55,11 @@ for u in users:
     prs.save_list_of_players(items, u)
     # set of players
     pl.save_players_list(pl.create_players_set(u), u)
-    totals, totals_easy, totals_null, totals_sd, totals_dd = c.get_all_stats(u)
     results[u] = c.get_all_stats(u)
 
 print('Data is generated.')
 
-with open(f'../output/all_stat_{datetime.timestamp(datetime.now())}.tsv', 'w', newline='') as f:
-    w = csv.writer(f, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    w.writerow(['Username', 'Type', 'W(%)', 'L(%)', 'W(#)', 'L(#)',
-                'W(%, 2p)', 'L(%, 2p)', 'W(#, 2p)', 'L(#, 2p)',
-                'W(%, 3p+)', 'L(%, 3p+)', 'W(#, 3p+)', 'L(#, 3p+)'])
-    for k, v in results.items():
-        for k1, t in v.items():
-            w.writerow([
-                k,
-                k1,
-                # r(t['total_p'][0]),
-                t['total_p'][0],
-                # r(t['total_p'][1]),
-                t['total_p'][1],
-                t['total_c'][0],
-                t['total_c'][1],
-                # r(t['total_2p_p'][0]),
-                t['total_2p_p'][0],
-                # r(t['total_2p_p'][1]),
-                t['total_2p_p'][1],
-                t['total_2p_c'][0],
-                t['total_2p_c'][1],
-                # r(t['total_3p_p'][0]),
-                t['total_3p_p'][0],
-                # r(t['total_3p_p'][1]),
-                t['total_3p_p'][1],
-                t['total_3p_c'][0],
-                t['total_3p_c'][1]]
-            )
+save_to_tsv(f'all_stats_{datetime.timestamp(datetime.now())}', results)
+save_to_tsv('up_to_date_stats', results)
 
 print('Time spent (in min):', round((time.time() - start) / 60, 2))
