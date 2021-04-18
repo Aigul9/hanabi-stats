6f272dbcfdc0160bd88cce62f14c82389946cf10
Aigul <aigul.sibgatulli@mail.ru>
2021-04-18 17:15:03 +0300

    tab test

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for u in users:
 print('Data is generated.')
 
 with open(f'../output/all_stat_{datetime.timestamp(datetime.now())}.csv', 'w', newline='') as f:
-    w = csv.writer(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    w = csv.writer(f, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     w.writerow(['Username', 'Type', 'W(%)', 'L(%)', 'W(#)', 'L(#)',
                 'W(%, 2p)', 'L(%, 2p)', 'W(#, 2p)', 'L(#, 2p)',
                 'W(%, 3p+)', 'L(%, 3p+)', 'W(#, 3p+)', 'L(#, 3p+)'])
