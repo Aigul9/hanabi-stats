b3a8cc5ff63729194996fd5eec30811466e28e02
Aigul <aigul.sibgatulli@mail.ru>
2021-04-19 10:16:29 +0300

    update totals

--- py/players_most_wl.py
+++ py/players_most_wl.py
@@ -19,10 +19,10 @@ def get_players_dict(username, players):
         # p_l_ratio = c.p(p_losses, p_total)
         ms = len(main_stats)
         easy_ratio, null_ratio, sd_ratio, dd_ratio =\
-            c.p(len(get_filtered_by_player(p, list_easy)), ms),\
-            c.p(len(get_filtered_by_player(p, list_null)), ms),\
-            c.p(len(get_filtered_by_player(p, list_sd)), ms),\
-            c.p(len(get_filtered_by_player(p, list_dd)), ms)
+            c.p(len(get_filtered_by_player(p, list_easy)), p_total),\
+            c.p(len(get_filtered_by_player(p, list_null)), p_total),\
+            c.p(len(get_filtered_by_player(p, list_sd)), p_total),\
+            c.p(len(get_filtered_by_player(p, list_dd)), p_total)
         results[p] = {'wl': p_ratio, 'total': p_total, 'easy': easy_ratio, 'null': null_ratio,
                       'sd': sd_ratio, 'dd': dd_ratio}
     return {k: v for k, v in sorted(results.items(), key=lambda item: item[1]['wl'], reverse=True)}
@@ -36,7 +36,7 @@ def save_players_dict(username, data):
     with open(f'../output/{username}_wl_by_players.tsv', 'w', newline='') as file:
         w = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         w.writerow([
-            'Player name', 'W/L(%)', 'Total games(#)', 'Easy', 'Null', 'Single dark', 'Double dark']
+            'Player name', 'W/L(%)', 'Total games', 'Easy', 'Null', 'Single dark', 'Double dark']
         )
         for k, v in data.items():
             w.writerow([
