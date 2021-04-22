1dbca340633322932d063b6e8f7e579686188ad9
Aigul <aigul.sibgatulli@mail.ru>
2021-04-22 10:20:38 +0300

    top - bottom

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import py.players as pl
 import py.calc as c
 import py.players_most_wl as wl
 from datetime import datetime
+from collections import defaultdict
 
 
 def r(num):
@@ -62,32 +63,37 @@ def save_wr(filename, data):
             ])
 
 
-def save_ranking(filename, data):
-    with open(f'../output/rank_{filename}.tsv', 'w', newline='') as file:
+def save_ranking(data):
+    with open('../output/rank.tsv', 'w', newline='') as file:
         w = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        w.writerow(['Username', 'Rank'])
+        w.writerow(['Username', 'Rank', 'Seek', 'Avoid'])
         for k, v in data.items():
             w.writerow([
                 k,
-                v
+                v[0],
+                ', '.join(v[1]),
+                ', '.join(v[2])
             ])
 
 
-def assign_weights(tb_list, global_list_1, global_list_w, global_type):
+def assign_weights(username, tb_list, global_type):
+    # print('global': )
     for pl in tb_list:
-        if pl[0] in global_list_1:
-            global_list_1[pl[0]] += 1
+        # global_ranking[pl[0]] = {'rank': 0, 'seek': [], 'avoid': []}
+        if pl[0] in global_ranking:
             if global_type == 'top':
-                global_list_w[pl[0]] += len(tb_list) - tb_list.index(pl)
+                global_ranking[pl[0]][0] += len(tb_list) - tb_list.index(pl)
+                global_ranking[username][1].append(pl[0])
             elif global_type == 'bottom':
-                global_list_w[pl[0]] += tb_list.index(pl) + 1
+                global_ranking[pl[0]][0] -= tb_list.index(pl) + 1
+                global_ranking[username][2].append(pl[0])
         else:
-            global_list_1[pl[0]] = 0
-            global_list_w[pl[0]] = 0
+            global_ranking[pl[0]] = []
+            global_ranking[pl[0]] = [0, [], []]
 
 
 def global_sort(global_list):
-    return {k: v for k, v in sorted(global_list.items(), key=lambda item: (-item[1], item[0]))}
+    return {k: v for k, v in sorted(global_list.items(), key=lambda item: (-item[1][0]))}
 
 
 start = time.time()
@@ -98,10 +104,7 @@ with open('../input/list_of_players.txt', 'r') as f:
 results = {}
 results_var = {}
 results_var_not = {}
-global_ranking_top_1 = dict.fromkeys(users, 0)
-global_ranking_top_weight = dict.fromkeys(users, 0)
-global_ranking_bottom_1 = dict.fromkeys(users, 0)
-global_ranking_bottom_weight = dict.fromkeys(users, 0)
+global_ranking = {k: [0, [], []] for k in users}
 top = 5
 for u in users:
     # # parsing
@@ -124,13 +127,16 @@ for u in users:
     # list_bottom_n = wl.get_bottom_n(10, list_for_top_10)
     list_top_n = wl.get_top_n(top, list_for_top_10)
     list_bottom_n = wl.get_bottom_n(top, list_for_top_10)
-    print(u)
-    print(list_top_n)
-    print(list_bottom_n)
-    assign_weights(list_top_n, global_ranking_top_1, global_ranking_top_weight, 'top')
-    assign_weights(list_bottom_n, global_ranking_bottom_1, global_ranking_bottom_weight, 'bottom')
+    # print(u)
+    # print(list_top_n)
+    # print(list_bottom_n)
+    assign_weights(u, list_top_n, 'top')
+    assign_weights(u, list_bottom_n, 'bottom')
+    # print(u)
+    # print(global_ranking)
 
 print('Data is generated.')
+# print(global_ranking)
 
 # save_to_tsv(f'all_stats_{datetime.timestamp(datetime.now())}', results)
 # save_to_tsv('up_to_date_stats', results)
@@ -138,10 +144,7 @@ print('Data is generated.')
 # save_wr('bga', results_var)
 # save_wr('non_speedrun', results_var_not)
 
-save_ranking(f'top_{top}_count', global_sort(global_ranking_top_1))
-save_ranking(f'top_{top}_weights', global_sort(global_ranking_top_weight))
-save_ranking(f'bottom_{top}_count', global_sort(global_ranking_bottom_1))
-save_ranking(f'bottom_{top}_weights', global_sort(global_ranking_bottom_weight))
+save_ranking(global_sort(global_ranking))
 
 print('End time:', datetime.now())
 print('Time spent (in min):', round((time.time() - start) / 60, 2))
