06ff0a8c9b9981fa59c002a26c9fc135d4d788b3
Aigul <aigul.sibgatulli@mail.ru>
2021-06-02 22:34:33 +0300

    purple looker

---
+++ py/purple_looker.py
@@ -0,0 +1,42 @@
+import requests
+import csv
+import calendar
+from itertools import groupby
+
+
+def open_stats(user):
+    url = f'https://hanab.live/history/{user}?api'
+    response = requests.get(url)
+    return response.json()
+
+
+def clear_2p(stats):
+    return [row for row in stats if int(row['options']['numPlayers']) != 2]
+
+
+def clear_speedruns(stats):
+    return [row for row in stats if not row['options']['speedrun']]
+
+
+def group_stats(stats):
+    groups = groupby(stats, lambda row: row['datetimeFinished'][:7])
+    grouped_stats = {}
+    for k, v in groups:
+        group = list(v)
+        grouped_stats[k] = len(group)
+    return {k: v for k, v in sorted(grouped_stats.items())}
+
+
+def save(grouped_stats, user):
+    with open(f'grouped_stats_{user}.csv', 'w', newline='') as f:
+        w = csv.writer(f, delimiter='\t')
+        for k, v in grouped_stats.items():
+            month = calendar.month_abbr[int(k[5:])]
+            year = k[:4]
+            w.writerow([f'{month} {year} - {v} games'])
+        w.writerow([f'Total: {sum(grouped_stats.values())}'])
+
+
+username = 'fey'
+data = group_stats(clear_2p(clear_speedruns(open_stats(username))))
+save(data, username)
