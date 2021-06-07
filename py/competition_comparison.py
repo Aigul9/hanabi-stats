0bf2d422473f48d3bffd20fdd5c44c08e1d28c51
Aigul <aigul.sibgatulli@mail.ru>
2021-06-07 11:29:56 +0300

    add column representation, fix row structure

--- py/competition_comparison.py
+++ py/competition_comparison.py
@@ -44,33 +44,67 @@ def generate_stats(ids):
     return stats
 
 
+def update_clue(seed):
+    seed[2] = seed[2] + seed[3]
+    del seed[3]
+    return seed
+
+
+def join_teams(teams):
+    return ', '.join(teams)
+
+
 def combine_dict(stats):
     combined_stats = {}
-    for k in stats[0].keys():
-        combined_stats[k] = []
-        for i in range(len(stats)):
-            combined_stats[k].append(stats[i][k])
+    for seed in stats[0].keys():
+        combined_stats[seed] = {}
+        for k in stats[0][seed].keys():
+            combined_part = []
+            # number of lists that needs to be combined
+            for i in range(len(stats)):
+                combined_part.append(stats[i][seed][k])
+            combined_stats[seed][k] = combined_part
     return combined_stats
 
 
-def save(all_stats):
-    with open(f'../output/comp_comparison.tsv', 'w', newline='') as f:
+def save_row(all_stats):
+    with open(f'../output/comp_comparison_row.tsv', 'w', newline='') as f:
         w = csv.writer(f, delimiter='\t')
         w.writerow(['Team', 'Game id', 'Turns', 'Play', 'Discard', 'Clue', 'Seed'])
         for k in all_stats[0].keys():
-            for i in range(len(all_stats)):
-                stats_list = all_stats[i][k]
-                stats_list['team'] = ', '.join(stats_list['team'])
-                stats_list[2] = stats_list[2] + stats_list[3]
-                del stats_list[3]
+            for s in all_stats:
+                stats_list = s[k]
+                stats_list['team'] = join_teams(stats_list['team'])
+                stats_list = update_clue(stats_list)
                 stats_list = list(stats_list.values())
                 stats_list.append(k)
                 w.writerow(stats_list)
 
 
+def save_column(combined_stats):
+    with open(f'../output/comp_comparison_col.tsv', 'w', newline='') as f:
+        w = csv.writer(f, delimiter='\t')
+        header = []
+        for i in range(1, len(list(combined_stats.values())[0]['team']) + 1):
+            header.append(f'Team {str(i)}')
+        w.writerow(header)
+        for k, v in combined_stats.items():
+            w.writerow(['Seed', k, k])
+            teams = []
+            for team in v['team']:
+                teams.append(join_teams(team))
+            w.writerow(['Teams', *teams])
+            w.writerow(['Game id', *v['game_id']])
+            w.writerow(['Turns', *v['turns']])
+            w.writerow(['Play', *v[0]])
+            w.writerow(['Discard', *v[1]])
+            clues = [[*v[2]], [*v[3]]]
+            w.writerow(['Clue', *[sum(c) for c in zip(*clues)]])
+
+
 lib_games, val_games = load_games()
 lib_stats = generate_stats(lib_games)
 val_stats = generate_stats(val_games)
-# print(lib_stats)
-save([lib_stats, val_stats])
+# save_row([lib_stats, val_stats])
+save_column(combine_dict([lib_stats, val_stats]))
 
