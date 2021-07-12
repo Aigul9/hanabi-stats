c95d4e0de670c09063136be4b191636e2feb3e98
Aigul <aigul.sibgatulli@mail.ru>
2021-07-12 10:52:41 +0300

    total scores by num of players

--- py/total_max_scores.py
+++ py/total_max_scores.py
@@ -1,15 +1,31 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
+import py.utils as ut
 
 
-def get_history_table(username):
-    url = 'https://hanab.live/scores/' + username
+def get_content(url, username):
     page = requests.get(url)
     if page.status_code != 200:
         print('Username is not valid:', username)
         exit()
-    soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup)
-    # return soup.find(id='history-table')
+    return BeautifulSoup(page.content, 'html.parser')
 
-get_history_table('Valetta6789')
+
+def get_digits(text):
+    return int(text.split()[0].replace('<td>', ''))
+
+
+def get_total_scores(username):
+    url = 'https://hanab.live/missing-scores/' + username
+    soup = get_content(url, username)
+    text = soup.find_all('td')
+    return [get_digits(str(t)) for t in text]
+
+
+users = ut.open_file('../input/list_of_players_notes.txt')
+scores = {}
+for u in users:
+    scores[u] = get_total_scores(u)
+scores = {k: v for k, v in sorted(scores.items(), key=lambda i: -i[1][5])}
+ut.save('total_max_scores', scores, ['Player', '2p', '3p', '4p', '5p', '6p', 'Total scores'])
