af90510a8b992ff678e3e99cd7840a4c4868a76d
Aigul <aigul.sibgatulli@mail.ru>
2021-07-16 07:21:10 +0300

    add labels games per month

--- py/query_processing.py
+++ py/query_processing.py
@@ -5,13 +5,12 @@ import py.utils as ut
 count_games = ut.open_tsv('../output/Result_3.tsv')[:-1]
 count = [int(r[2]) for r in count_games]
 time = [', '.join([r[0]] + [r[1]]) for r in count_games]
-print(count)
 plt.figure(figsize=(40, 5))
 plt.xlabel('Month, Year')
 plt.ylabel('Count')
 plt.scatter(time, count)
-# for i, txt in enumerate(y):
-#     plt.annotate(txt, (x[i], n[i]))
+for i, txt in enumerate(count):
+    plt.annotate(txt, (time[i], count[i]))
 plt.title('Games by months')
 plt.plot(time, count)
 plt.savefig(f'../output/games_per_month.png')
