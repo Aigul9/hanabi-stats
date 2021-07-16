643a75e1fb82857b677250d8d727b3bab4236ae0
Aigul <aigul.sibgatulli@mail.ru>
2021-07-16 07:29:10 +0300

    speedruns tendency

--- py/query_processing.py
+++ py/query_processing.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 
 import py.utils as ut
 
-count_games = ut.open_tsv('../output/Result_3.tsv')[:-1]
+count_games = ut.open_tsv('../output/Result_4.tsv')[:-1]
 count = [int(r[2]) for r in count_games]
 time = [', '.join([r[0]] + [r[1]]) for r in count_games]
 plt.figure(figsize=(40, 5))
@@ -13,4 +13,4 @@ for i, txt in enumerate(count):
     plt.annotate(txt, (time[i], count[i]))
 plt.title('Games per month')
 plt.plot(time, count)
-plt.savefig(f'../output/games_per_month.jpg')
+plt.savefig(f'../output/games_per_month_speedruns.jpg')
