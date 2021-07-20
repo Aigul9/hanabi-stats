ed79ff22741ea18305a3b5d78369509a7e2155e2
Aigul <aigul.sibgatulli@mail.ru>
2021-07-20 18:50:43 +0300

    clues testing

--- py/clues.py
+++ py/clues.py
@@ -1,24 +1,24 @@
 import csv
 
 import py.utils as u
-from database.db_connect import session
-from database.db_schema import Game, GameAction
+from database.db_connect import session, Game, GameAction, Variant
 
 
-users = ['Dr_Kakashi']
+users = ['Dr_Kakashi', 'Valetta6789', 'Lanvin']
 # [game_id, player_count, clues]
 results = {k: [] for k in users}
 for user in users:
-    stats = u.open_stats(user)
-    for s in stats:
-        # for s in [99104]:
-        game_id = s['id']
-        # game_id = s
+    games = session.query(Game) \
+        .join(Variant) \
+        .filter(Game.players.any(user)) \
+        .filter(Game.score == Variant.max_score) \
+        .all()
+    for g in games:
+        game_id = g.game_id
         print(game_id)
-        players, starting_player = session.query(Game.players, Game.starting_player).filter(Game.game_id == game_id).first()
+        players = g.players
         players_count = len(players)
-        if starting_player is not None:
-            players = (players[starting_player:] + players[:starting_player])
+        players = (players[g.starting_player:] + players[:g.starting_player])
         actions = session.query(GameAction).filter(GameAction.game_id == game_id).all()
         clues = {k: 0 for k in players}
         for i in range(len(actions)):
@@ -26,13 +26,12 @@ for user in users:
                 clues[players[i % players_count]] += 1
         max_users = [k for k, v in clues.items() if v == max(clues.values())]
         if len(max_users) == 1 and max_users[0] == user:
-            results[u].append([game_id, players_count, clues[user]])
+            results[user].append([game_id, players_count, clues[user]])
 
-
-for c in range(2, 7):
-    with open(f'../output/Dr_Kakashi_clues_{c}p.tsv', 'a', encoding='utf-8', newline='') as file:
-        w = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE, escapechar='\\')
-        w.writerow(['Game id', 'Player count', 'Number of clues'])
-    data = sorted([i for v in results.values() for i in v if i[1] == c], key=lambda x: -x[2])
-    for r in data:
-        w.writerow([*r])
+    for c in range(2, 7):
+        with open(f'../output/clues/{user}_clues_{c}p.tsv', 'a', encoding='utf-8', newline='') as file:
+            w = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE, escapechar='\\')
+            w.writerow(['Game id', 'Player count', 'Number of clues'])
+            data = sorted([v for v in results[user] if v[1] == c], key=lambda x: -x[2])
+            for r in data:
+                w.writerow([*r])
