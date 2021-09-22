ecfcdf80b4438dfca66f3b78741b4fd931203262
Aigul <aigul.sibgatulli@mail.ru>
2021-09-22 21:05:06 +0300

    add date to errors.log

--- database/migration.py
+++ database/migration.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from json.decoder import JSONDecodeError
 
 import requests
@@ -11,6 +12,7 @@ import database.db_load as d
 
 last_id = d.session.query(func.max(Game.game_id)).scalar()
 # last_id = 627743
+logger.info(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
 logger.info(f'last id: {last_id}')
 req_session = requests.Session()
 histories = {}
@@ -40,6 +42,7 @@ while True:
         d.load_slots(db_game)
         d.session.commit()
     else:
-        logger.error(f'skip: {last_id}')
         d.session.close()
+        logger.error(f'end: {g_id}')
+        logger.info(datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
         break
