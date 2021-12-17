7846a27a80a23ba1c532a8a2090d8874b72bc3d2
Aigul <aigul.sibgatulli@mail.ru>
2021-12-17 20:00:00 +0300

    refactor and document times

--- py/time_spent.py
+++ py/time_spent.py
@@ -1,52 +1,84 @@
 import csv
-from datetime import datetime
 from itertools import groupby
 
+import py.utils as u
+from database.db_connect import session, Game, Player
 
-def save(data):
-    with open(f'output/time/times_spent.tsv', 'w', encoding='utf-8', newline='') as file:
+
+def save(player_times_dict):
+    """Saves total time spent on games by player into a tsv file.
+
+    Parameters
+    ----------
+    player_times_dict : dict
+        Dictionary containing player name, total days, hours, minutes per game, hours per day
+    """
+    with open(f'../output/time/times_spent.tsv', 'w', encoding='utf-8', newline='') as file:
         w = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE, escapechar='\\')
-        w.writerow(['Player', 'Days', 'Hours', 'Per game (in min)', 'Per day (in h, incl. 0)', 'Per day (in h, excl. 0)'])
-        for k, v in data.items():
-            w.writerow([k, v[0]['days'], v[0]['hours'], v[1], v[2], v[3]])
+        w.writerow(
+            ['Player', 'Days', 'Per game (in min)', 'Per day (in h)'])
+        for k, v in player_times_dict.items():
+            w.writerow([k, v[0]['days'], v[1], v[2]])
 
 
-def group_stats(data):
-    groups = groupby(data, lambda row: row['datetimeFinished'][:10])
-    keys = []
-    for k, v in groups:
-        keys.append(k)
-    return len(keys)
+def group_stats(games):
+    """Calculates number of days spent on the website.
+
+    Parameters
+    ----------
+    games : list
+        List of player's games
+
+    Returns
+    -------
+    int
+        Number of days
+    """
+    groups = groupby(games, lambda row: row.date_time_finished.date())
+    return len(list(groups))
 
 
-# {'Valetta6789': [time, num_games, num_days_since_joined]}
-# time in sec
-# time / num_games (in min)
-# time / num_days_since_joined (in h)
+def get_times(username):
+    """Calculates number of minutes per game and hours per day.
 
 
-def get_times(stats):
-    days_2 = group_stats(stats)
-    date = stats[len(stats) - 1]['datetimeStarted']
-    try:
-        d_joined = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
-    except ValueError:
-        d_joined = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
-    days_1 = (datetime.now() - d_joined).days
-    times = [0, len(stats), days_1, days_2]
-    for s in stats:
-        try:
-            d_start = datetime.strptime(s['datetimeStarted'], '%Y-%m-%dT%H:%M:%S.%fZ')
-            d_finish = datetime.strptime(s['datetimeFinished'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        except ValueError:
-            d_start = datetime.strptime(s['datetimeStarted'], '%Y-%m-%dT%H:%M:%SZ')
-            d_finish = datetime.strptime(s['datetimeFinished'], '%Y-%m-%dT%H:%M:%SZ')
-        diff = d_finish - d_start
+    Parameters
+    ----------
+    username : str
+        Player name
+
+    Returns
+    -------
+    times : list
+        List of total seconds, minutes per game and hours per day
+    """
+    games = session.query(Game) \
+        .filter(Game.players.any(username))\
+        .order_by(Game.game_id)\
+        .all()
+    days = group_stats(games)
+    times = [0, len(games), days]
+    for game in games:
+        diff = game.date_time_finished - game.date_time_started
         times[0] += diff.total_seconds()
     # per game
     times[1] = times[0] / times[1] / 60
-    # per day
-    times[2] = times[0] / times[2] / 3600
     # per day excl. days with 0 games
-    times[3] = times[0] / times[3] / 3600
+    times[2] = times[0] / times[2] / 3600
     return times
+
+
+if __name__ == "__main__":
+    users = session.query(Player.player).all()
+    users_times = {}
+    for user in users:
+        user = user[0]
+        users_times[user] = get_times(user)
+
+    users_times = {k: [
+        u.convert_sec_to_day(v[0]),
+        round(v[1]),
+        round(v[2], 1)
+    ]
+        for k, v in u.sort(users_times, 0).items()}
+    save(users_times)
