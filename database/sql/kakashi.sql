435c12a7d26d9fda9b60d8b2e90af652d4855280
Aigul <aigul.sibgatulli@mail.ru>
2022-01-10 01:58:24 +0300

    3p no var games and seeds requested by kakashi

---
+++ database/sql/kakashi.sql
@@ -0,0 +1,33 @@
+select g.game_id, max(turn_action)from games g
+join card_actions ca on g.game_id = ca.game_id
+where num_players = 3
+  and end_condition = 1
+  and variant = 'No Variant'
+  and g.game_id not in (select * from bugged_games)
+  and speedrun is false
+  and all_or_nothing is false
+  and one_extra_card is false
+  and one_less_card is false
+group by g.game_id;
+
+select * from card_actions where game_id = 4761;
+select * from game_actions where game_id = 4761;
+
+select * from bugged_games;
+
+--seeds
+select distinct seed, max(turn_action)from games g
+join card_actions ca on g.game_id = ca.game_id
+where num_players = 3
+  and end_condition = 1
+  and variant = 'No Variant'
+  and g.game_id not in (select * from bugged_games)
+  and speedrun is false
+  and all_or_nothing is false
+  and one_extra_card is false
+  and one_less_card is false
+group by seed;
+
+select max(game_id) from games;
+
+select max(game_id) from tags;
