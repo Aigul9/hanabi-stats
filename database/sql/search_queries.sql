689d6d364cf9d9cb93938adecae9808c99f0e1b5
Aigul <aigul.sibgatulli@mail.ru>
2022-01-10 12:10:25 +0300

    terminated 2p games

--- database/sql/search_queries.sql
+++ database/sql/search_queries.sql
@@ -468,7 +468,7 @@ where rank = 1
 order by 2 desc;
 
 --group by num_players
-select *, "2p_ratio" + "3p_ratio" + "4p_ratio" + "5p_ratio" + "6p_ratio" as check
+select *, "2p_ratio" + "3p_ratio" + "4p_ratio" + "5p_ratio" + "6p_ratio" as "check"
 from (select player,
              round(p2 * 1.0 / total, 2) as "2p_ratio",
              round(p3 * 1.0 / total, 2) as "3p_ratio",
@@ -586,3 +586,33 @@ and player != 'Valetta6789'
 and date_time_started < '2020-01-01'
 group by player
 order by 2 desc;
+
+--number of terminated games for any pair of players which has more than 100 games
+with pair
+    as (select pl1.player as player1,
+               pl2.player as player2
+        from players_list pl1,
+             players_list pl2
+        --remove duplicates
+        where pl1.player < pl2.player
+    )
+select player1,
+       player2,
+       total,
+       term,
+       concat(round(term * 100.0 / total), '%') as perc
+from (select player1,
+             player2,
+             count(*)                                  as total,
+             --terminated games
+             count(*) filter (where end_condition = 4) as term
+      from pair p
+               join games g
+                    on p.player1 = any (players)
+                        and p.player2 = any (players)
+      where num_players = 2
+        and speedrun is false
+      group by player1, player2
+     ) t
+where total >= 100
+order by 5 desc;
