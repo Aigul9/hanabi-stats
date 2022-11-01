b910270bc21f7c4bd7b7f41c439721e371e18cd4
Aigul <aigul.sibgatulli@mail.ru>
2022-11-01 21:39:24 +0300

    rename addicts

--- py/time/top_10_by_hours.py
+++ py/time/top_10_by_hours.py
@@ -15,12 +15,12 @@ from sqlalchemy import text
 import py.utils as u
 
 if __name__ == "__main__":
-    path = 'output/time/monthly_hanabaddicts'
+    path = 'output/time/top_10_by_hours'
     header = ['Player', 'Hours']
     date = datetime.now()  # .replace(2022, 9)
-    month = date.month
+    month = date.month - 1
     year = date.year
-    sub_path = f'output/time/monthly_hanabaddicts/{year}_{date.strftime("%m")}'
+    sub_path = f'output/time/top_10_by_hours/{year}_{month}'
 
     sql_file = u \
         .read_file(f'../../database/sql/{path}.sql') \
