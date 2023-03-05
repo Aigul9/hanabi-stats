ef1078b0a2bfa1b7eabafa41276a7c8108ca78cf
Aigul <46625461+Aigul9@users.noreply.github.com>
2023-03-05 15:25:57 +0300

    fix kimbi's header

--- database/sql/requests/kimbi.sql
+++ database/sql/requests/kimbi.sql
@@ -1,9 +1,7 @@
 select
     'kimbifille' as player,
     extract(year from date_time_started) as year,
-    TO_CHAR(
-       TO_DATE (extract(month from date_time_started)::text, 'MM')
-       , 'Month') as month,
+    extract(month from date_time_started) as month,
     round(sum(extract(epoch from date_time_finished - date_time_started) / 3600)) as hours
 from games
 where 'kimbifille' = any(players)
